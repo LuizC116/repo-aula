402df509b9feb4a1cd4b8d65a8c345ef03de8826
LuizC116 <luizc116@unoeste.edu.br>
2026-03-20 20:29:47 -0300

    Add - Funções operações matemática javascript

--- funcoes.js
+++ funcoes.js
@@ -1,3 +1,15 @@
 function soma(n1, n2){
     return n1+n2;
 }
+
+function subtracao(n1,n2) {
+    return n1-n2;
+}
+
+function multplicacao(n1, n2) {
+    return n1*n2;
+}
+
+function divisao(n1, n2) {
+    return n1/n2;
+}
